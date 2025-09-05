ce05b2bc67add7160e6abd3a5ee556f0eca4a0a5
dimassugase <dimasagussetiawn08@gmail.com>
2025-09-05 14:29:53 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Portfolio from './Portfolio'; // Make sure this matches your actual file name
+
+
+function App() {
+  return (
+    <div>
+      <Portfolio />
+    </div>
+  );
+}
+
+export default App;
